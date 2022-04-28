85a060501b871f97980a0167cf8e3ec34aa8ff5e
Justin Vongdara <vongdara16@gmail.com>
2022-04-28 12:25:03 -0400

    change recipe card

--- src/App.jsx
+++ src/App.jsx
@@ -14,32 +14,32 @@ const App = () => {
   // console.log(user)
   const [recipes, setRecipes] = useState([])
 
-  const [recipes, setRecipes] = useState([
-    {
-      name: "Cheesy Beef Scramble",
-      instructions: "Recipe 1 instructions",
-      ingredients: "Recipe 1 ingredients",
-      mainIngredient: "Beef",
-      category: "Breakfast",
-      image: "https://www.thecookierookie.com/wp-content/uploads/2019/08/cheesy-beef-breakfast-scramble-7-of-12.jpg"
-    },
-    {
-      name: "Pork Tenderloin Fajitas",
-      instructions: "Recipe 2 instructions",
-      ingredients: "Recipe 2 ingredients",
-      mainIngredient: "Pork",
-      category: "Lunch",
-      image: "https://www.tasteofhome.com/wp-content/uploads/2018/01/Pork-Tenderloin-Fajitas_EXPS_SDFM19_30888_B10_19_4b-3.jpg"
-    },
-    {
-      name: "Creamy Tuscan Chicken",
-      instructions: "Recipe 3 instructions",
-      ingredients: "Recipe 3 ingredients",
-      mainIngredient: "Chicken",
-      category: "Dinner",
-      image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20191011-creamy-tuscan-chicken-delish-ehg-2517-1571259200.jpg"
-    }
-  ])
+  // const [recipes, setRecipes] = useState([
+  //   {
+  //     name: "Cheesy Beef Scramble",
+  //     instructions: "Recipe 1 instructions",
+  //     ingredients: "Recipe 1 ingredients",
+  //     mainIngredient: "Beef",
+  //     category: "Breakfast",
+  //     image: "https://www.thecookierookie.com/wp-content/uploads/2019/08/cheesy-beef-breakfast-scramble-7-of-12.jpg"
+  //   },
+  //   {
+  //     name: "Pork Tenderloin Fajitas",
+  //     instructions: "Recipe 2 instructions",
+  //     ingredients: "Recipe 2 ingredients",
+  //     mainIngredient: "Pork",
+  //     category: "Lunch",
+  //     image: "https://www.tasteofhome.com/wp-content/uploads/2018/01/Pork-Tenderloin-Fajitas_EXPS_SDFM19_30888_B10_19_4b-3.jpg"
+  //   },
+  //   {
+  //     name: "Creamy Tuscan Chicken",
+  //     instructions: "Recipe 3 instructions",
+  //     ingredients: "Recipe 3 ingredients",
+  //     mainIngredient: "Chicken",
+  //     category: "Dinner",
+  //     image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20191011-creamy-tuscan-chicken-delish-ehg-2517-1571259200.jpg"
+  //   }
+  // ])
 
   const handleLogout = () => {
     authService.logout()
